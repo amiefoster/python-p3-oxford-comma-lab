50da7196aeb18e9c6f0dfaefa3572a79b06bb85c
Amie Foster <amie.n.foster@gmail.com>
2023-02-07 11:43:54 -0600

    completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    print(items)
+    #listItems = items.split()
+    if len(items) == 1:
+        return ' '.join(items)
+    elif len(items) == 2:
+        return ' and '.join(items)
+    else:
+        #The way it works is listed[-1] gets the last value from the list. 
+        #We use = to assign this value to listed[-1][:-1], which is a slice of the last word from the list with everything before the last character.
+        return ('{}, and {}'.format(', '.join(items[:-1]), items[-1]))
